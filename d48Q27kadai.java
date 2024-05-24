5d76af0ee3891e395f805383183260e765abc71a
itc-s23014 <s23014@std.it-college.ac.jp>
2024-05-24 12:33:01 +0900

    kadai fin

---
+++ d48Q27kadai.java
@@ -0,0 +1,11 @@
+//s23014
+//インクリメントを使用して無限ループしないようにした
+public class d48Q27kadai {
+	public static void main(String[] args) {
+		int i = 0;
+		while (i <= 6) {
+			++i;
+			System.out.print(i);
+		}
+	}
+}
