f10a3c8dc77c1be4b47c205e3c56defe72b307d7
itc-s23014 <s23014@std.it-college.ac.jp>
2024-06-14 04:26:10 +0000

    Q26kadaiアレンジ

---
+++ d49Q26kadai.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class d49Q26kadai {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double n = scanner.nextDouble();
+        d49Q26kadai obj = new d49Q26kadai();
+        obj.show(n);
+    }
+    void show(double n) {
+        System.out.println("value is"+n);
+    }
+}
