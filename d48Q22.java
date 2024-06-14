8dd82f9296e9ebd325f27326086e620e6436fe39
itc-s23014 <s23014@std.it-college.ac.jp>
2024-06-14 09:52:45 +0900

    作業前バックアップ

---
+++ d48Q22.java
@@ -0,0 +1,14 @@
+enum Connection {OFFLINE,ONLINE}
+
+class d48Q22 {
+    public static void main(String[] args) {
+    
+    Connection s = OFFLINE;
+    if (s == Connection.OFFLINE){
+        s = Connection.ONLINE;
+        
+}
+    System.out.println(s);
+
+}
+}
