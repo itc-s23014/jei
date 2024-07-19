4ac4b7df4c7825a6481517b18b569cddac652290
itc-s23014 <s23014@std.it-college.ac.jp>
2024-07-19 03:40:13 +0000

    kadai0719 2

---
+++ d51Q26kadai.java
@@ -0,0 +1,17 @@
+//s23014
+//ラムダ式を使いました
+public class d51Q26kadai{
+    public static void main(String[] args) {
+             int n = 1;
+             String s = " ";
+        switch (n) {
+
+            case   1  -> System.out.println("one");
+
+            case   2  -> System.out.println("two");
+
+            default -> System.out.println("?");
+
+        }
+    }
+}
